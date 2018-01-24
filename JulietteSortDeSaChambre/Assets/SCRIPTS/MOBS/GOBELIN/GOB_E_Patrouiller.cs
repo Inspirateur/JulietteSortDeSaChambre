ea65e2d875d1fc29587e90586f514a69c66ff285
Fiesta87 <jb@pommeret.fr>
2018-01-24 17:22:55 -0500

    ajout etat GOB_E_Patrouiller début et mis à jour du gobelin fictif de test

---
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/GOBELIN/GOB_E_Patrouiller.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOB_E_Patrouiller : IA_Etat {
+
+    public float vitesse;
+	public float delaisAChaqueArret;
+
+	public IA_PointInteret[] chemin;
+
+	public AudioClip sonArret;
+
+    private int indiceCheminActuel;
+	private float delaisActuel;
+	private bool enChemin;
+	private int indiceDernierPointRejoint;
+
+    // Use this for initialization
+    void Start () {
+        base.init(); // permet d'initialiser l'état, ne pas l'oublier !
+		this.delaisActuel = 0.0f;
+		indiceDernierPointRejoint = -1;
+    }
+
+    public override void entrerEtat()
+	{
+//        setAnimation("walking");
+		indiceCheminActuel = indiceDernierPointRejoint;
+		nav.enabled = true;
+        suivreChemin();
+		this.enChemin = true;
+    }
+
+    public override void faireEtat()
+    {
+//		if(agent.princesseReperee()) {
+//			changerEtat(this.GetComponent<gob_E_poursuite>());
+//		}
+//
+//		else if (enChemin) {
+		if (enChemin) {
+
+			if (agent.destinationCouranteAtteinte ()) {
+
+				indiceDernierPointRejoint = indiceCheminActuel;
+//				setAnimation("searching");
+				enChemin = false;
+				this.delaisActuel = Time.time + this.delaisAChaqueArret;
+				agent.getSoundEntity().playOneShot(sonArret, 1.0f);
+			}
+		} else if (Time.time > this.delaisActuel) {
+
+//			setAnimation("walking");
+			suivreChemin ();
+			enChemin = true;
+
+		} else {
+
+//			if(agent.princesseRepereeAvecAttention()) {
+//				changerEtat(this.GetComponent<gob_E_poursuite>());
+//			}
+		}
+    }
+
+    public override void sortirEtat()
+    {
+        
+    }
+
+	public override void subirDegats(int valeurDegats, Vector3 hitPoint)
+	{
+
+	}
+
+    private void suivreChemin()
+	{
+		indiceCheminActuel = (indiceCheminActuel + 1) % chemin.Length;
+        agent.definirDestination(chemin[indiceCheminActuel].transform.position);
+        nav.speed = vitesse;
+    }
+}
