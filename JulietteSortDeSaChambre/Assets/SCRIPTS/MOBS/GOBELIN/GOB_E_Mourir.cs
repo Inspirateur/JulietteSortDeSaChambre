bdf8d47eb90a2a665d5dae030d7366c27085810e
Fiesta87 <jb@pommeret.fr>
2018-01-26 11:52:33 -0500

    recuperation du script IA_GOB_Mourir et debut refactoring

---
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/GOBELIN/GOB_E_Mourir.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOB_E_Mourir : IA_Etat {
+
+	public float delaiAvantDisparition;
+	public AudioClip sonMort;
+
+	private float actualDelai;
+	private bool sonJoue;
+
+	// Use this for initialization
+	void Start()
+	{
+		base.init(); // permet d'initialiser l'état, ne pas l'oublier !
+
+		// ne pas initialiser vos autres variables ici, utiliser plutôt la méthode entrerEtat()
+	}
+
+	public override void entrerEtat()
+	{
+		setAnimation(GOB_Animations.MOURIR);
+		rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+		actualDelai = Time.time + delaiAvantDisparition;
+		sonJoue = false;
+	}
+
+	public override void faireEtat()
+	{
+		if (!agent.getSoundEntity().isPlaying() && !sonJoue){
+			agent.getSoundEntity().playOneShot(sonMort,1.0f);
+			sonJoue = true;
+		}
+		if(Time.time >= actualDelai){
+			this.gameObject.SetActive (false);
+		}
+	}
+
+	public override void sortirEtat()
+	{
+
+	}
+
+	public override void subirDegats(int valeurDegats, Vector3 hitPoint)
+	{
+
+	}
+}
