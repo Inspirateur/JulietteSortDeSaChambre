a69113b5dc5e8f53e8a588c3538de5a48c67b3a9
Fiesta87 <jb@pommeret.fr>
2018-01-28 17:15:15 -0500

    utilisation des 3 animations d'attaques du gobelin et refactoring

--- JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/GOBELIN/GOB_E_AttaquerEmpaler.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/GOBELIN/GOB_E_AttaquerEmpaler.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GOB_E_AttaquerEmpaler : IA_Etat {
 
-	public float distanceParcourue;
+	public int degatsParAttaque;
 	public float forceRecule;
+	public float distanceParcourue;
+	public float vitesse;
 	public AudioClip sonAttaque;
 
 	private bool degatsAttaqueEffectues;
 	private IA_TriggerArme colliderArme;
+	private int numAttaque;
 
 	// Use this for initialization
 	void Start()
@@ -26,27 +29,71 @@ public class GOB_E_AttaquerEmpaler : IA_Etat {
 		degatsAttaqueEffectues = false;
 		setAnimation (GOB_Animations.ATTAQUER_EMPALER);
 		nav.enabled = true;
-		agent.definirDestination (this.transform.position + this.transform.forward * distanceParcourue);
+		nav.speed = vitesse;
+		agent.definirDestination (this.transform.position + this.transform.forward * distanceParcourue * 0.33f);
+		numAttaque = 1;
 	}
 
 	public override void faireEtat()
 	{
-		if (!agent.isActualAnimation(GOB_Animations.COMBATTRE)) { // l'attaque puissante est toujours en cours
+		
+		switch (numAttaque) {
 
-			if (!degatsAttaqueEffectues && colliderArme.IsPrincesseTouchee ()) {
-
-//				princesseVie.blesser (degatsAttaquePuissante, this.gameObject, forceReculeAttaquePuissante);
-				degatsAttaqueEffectues = true;
-				Debug.Log ("princesse touchée par empalement");
+		case 1:
+			if (!agent.isActualAnimation (GOB_Animations.ATTAQUER_EMPALER + "2")) { // l'attaque 1 est toujours en cours
+				testerAttaque();
+			} else {
+				numAttaque++;
+				degatsAttaqueEffectues = false;
+				agent.definirDestination (this.transform.position + this.transform.forward * distanceParcourue * 0.33f);
+				setAnimation (GOB_Animations.COMBATTRE);
 			}
+			break;
 
+		case 2:
+			if (!agent.isActualAnimation (GOB_Animations.ATTAQUER_EMPALER + "3")) { // l'attaque 1 est toujours en cours
+				testerAttaque();
+			} else {
+				numAttaque++;
+				degatsAttaqueEffectues = false;
+				agent.definirDestination (this.transform.position + this.transform.forward * distanceParcourue * 0.33f);
+			}
+			break;
+
+		case 3:
+			if (!agent.isActualAnimation (GOB_Animations.COMBATTRE)) { // l'attaque 1 est toujours en cours
+				testerAttaque();
+			} else {
+				changerEtat(this.GetComponent<GOB_E_Combattre>());
+			}
+			break;
+		}
+
+
+		/*
+
+		if (numAttaque == 1){
+
+			if (!agent.isActualAnimation (GOB_Animations.ATTAQUER_EMPALER + "2")) { // l'attaque 1 est toujours en cours
+
+
+			}
 		} else {
 			changerEtat(this.GetComponent<GOB_E_Combattre>());
-		}
+		}*/
 	}
 
 	public override void sortirEtat()
 	{
 		nav.enabled = false;
+	}
+
+	private void testerAttaque(){
+		if (!degatsAttaqueEffectues && colliderArme.IsPrincesseTouchee ()) {
+
+//			princesseVie.blesser (degatsAttaquePuissante, this.gameObject, forceReculeAttaquePuissante);
+			degatsAttaqueEffectues = true;
+			Debug.Log ("touché attaque " + numAttaque);
+		}
 	}
 }
