b20a44b0660997b6cdd44989c7bc7c2e66b88c6c
Lucas <kitolex.soluce@gmail.com>
2018-02-02 18:42:08 -0500

    correction princesse objetProgression

---
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/PrincesseObjetProgression.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrincesseObjetProgression : MonoBehaviour {
+
+	public Dictionary<EnumObjetProgression,int> listObjet;
+
+	// Use this for initialization
+	void Start () {
+		listObjet = new Dictionary<EnumObjetProgression,int>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+	//modif pour sourcetree
+	public void addItem(EnumObjetProgression objetProgression){
+		if (listObjet.ContainsKey (objetProgression)) {
+			listObjet [objetProgression]++;
+		} else {
+			listObjet.Add (objetProgression,1);
+		}
+
+	}
+
+	public void removeItem(EnumObjetProgression objetProgression){
+		
+		if (listObjet [objetProgression] == 1) {
+			listObjet.Remove (objetProgression);
+		} else {
+			listObjet [objetProgression]--;
+		}
+	}
+}
+
+public enum EnumObjetProgression
+{
+	caisse,
+	key,
+}
