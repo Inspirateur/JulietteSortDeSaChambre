83ce6a188e44e778615978c0440be2e96cb9313b
Téo Orthlieb <teo.orthlieb@gmail.com>
2018-02-06 17:20:01 -0500

    le gobelin rejoint désormais le point le plus proche en entrant dans l'état patrouiller

--- JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/GOBELIN/GOB_E_Patrouiller.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/GOBELIN/GOB_E_Patrouiller.cs
@@ -23,11 +23,23 @@ public class GOB_E_Patrouiller : IA_Etat {
     void Start () {
         base.init(); // permet d'initialiser l'état, ne pas l'oublier !
 		this.delaisActuel = 0.0f;
-		indiceDernierPointRejoint = -1;
     }
 
     public override void entrerEtat()
 	{
+		//On cherche le point le plus proche
+		float minDist = float.MaxValue;
+		float minId = 0;
+		float dist;
+		for (int i = 0; i < chemin.Length; ++i) {
+			dist = (agent.transform.position - chemin [i].transform.position).magnitude;
+			if (dist < minDist) {
+				minId = i;
+				minDist = dist;
+			}
+		}
+		indiceDernierPointRejoint = (int)minId - 1;
+		Debug.Log ("entrerEtat() du gobelin, indiceDernierPointRejoint = " + indiceDernierPointRejoint);
 		setAnimation(GOB_Animations.MARCHER);
 		indiceCheminActuel = indiceDernierPointRejoint;
 		nav.enabled = true;
@@ -68,8 +80,10 @@ public class GOB_E_Patrouiller : IA_Etat {
 
     private void suivreChemin()
 	{
+		Debug.Log ("suivreChemin() du gobelin, indiceCheminActuel avant modif = " + indiceCheminActuel);
 		indiceCheminActuel = (indiceCheminActuel + 1) % chemin.Length;
-        agent.definirDestination(chemin[indiceCheminActuel].transform.position);
+		Debug.Log ("suivreChemin() du gobelin, indiceCheminActuel apres modif = " + indiceCheminActuel);
+		agent.definirDestination(chemin[indiceCheminActuel].transform.position);
         nav.speed = vitesse;
     }
 }
