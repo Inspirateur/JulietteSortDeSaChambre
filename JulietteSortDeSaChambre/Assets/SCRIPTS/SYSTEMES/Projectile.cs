5a7c6adfd5f93b74c6e7703b4623019b77027d3c
Fiesta87 <jb@pommeret.fr>
2018-02-14 18:16:19 -0500

    ajout feedback visuel et audio lors de la collision du projectile magique

--- JulietteSortDeSaChambre/Assets/SCRIPTS/SYSTEMES/Projectile.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/SYSTEMES/Projectile.cs
@@ -9,6 +9,7 @@ public class Projectile : MonoBehaviour {
 	public int degats;
 	public float recul;
 	public float dureeDeVie;
+	public GameObject effetCollision;
 
 	private float timerVie;
 	private Vector3 destination;
@@ -29,20 +30,20 @@ public class Projectile : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		
 		if(this.decorTouche(other)){
-			Destroy(this.gameObject);
+			this.detruireCollision();
 		}
 		else if(ami){
 			if (other.tag.Equals ("Mob")){
 				IA_Agent mobTouche = other.gameObject.GetComponent<IA_Agent> ();
 				Vector3 hitPoint = other.ClosestPoint (this.transform.position);
 				mobTouche.subirDegats (degats, hitPoint);
-				Destroy(this.gameObject);
+				this.detruireCollision();
 			}
 		}
 		else {
 			if (other.tag.Equals ("Player")){
 				GameObject.FindGameObjectWithTag("Player").GetComponent<PrincesseVie>().blesser (degats, this.gameObject, recul);
-				Destroy(this.gameObject);
+				this.detruireCollision();
 			}
 		}
 	}
@@ -54,5 +55,10 @@ public class Projectile : MonoBehaviour {
 
 	private bool decorTouche(Collider other){
 		return other.tag.Equals ("sol") || other.tag.Equals ("wall") || other.tag.Equals ("Decor");
+	}
+
+	private void detruireCollision(){
+		Instantiate(this.effetCollision, this.transform.position, this.transform.rotation);
+		Destroy(this.gameObject);
 	}
 }
