c80c6b0341d37cc80abec56408d07be8099e70e4
Lupusanghren <brisingr34130@gmail.com>
2018-02-16 10:58:53 -0500

    Ajout du cooldown, correction du sphre collider du pouvoir

--- JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/PrincessePouvoirGlace.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/PrincessePouvoirGlace.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PrincessePouvoirGlace : PrincessePouvoir
 {
+    private bool canPower;
 
     private SphereCollider sphereCollider;
     //public float icePowerDuration;
@@ -16,15 +17,18 @@ public class PrincessePouvoirGlace : PrincessePouvoir
         this.Nom = "Pouvoir de la glace";
         this.Description = "Gèle les ennemis devant vous pendant " + duration + " secondes";
         sphereCollider = GetComponent<SphereCollider>();
+        canPower = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (InputManager.GetButtonDown("pouvoirGlace") || Input.GetAxis("pouvoirGlace")<-0.75f)
+        if ((InputManager.GetButtonDown("pouvoirGlace") || Input.GetAxis("pouvoirGlace")<-0.75f)&& canPower)
         {
             sphereCollider.enabled = true;
+            canPower = false;
             StartCoroutine(WaitforIcePower());
+            StartCoroutine();
         }
     }
 
@@ -32,6 +36,13 @@ public class PrincessePouvoirGlace : PrincessePouvoir
     {
         yield return new WaitForSeconds(0.05f);
         sphereCollider.enabled = false;
+
+    }
+
+    IEnumerator WaitforUseIcePower()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canPower = true;
 
     }
 
