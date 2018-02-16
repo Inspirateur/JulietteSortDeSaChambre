b9305736288d5fdb12896e9d3c2b2bf5f3e222c2
Fiesta87 <jb@pommeret.fr>
2018-02-15 22:09:21 -0500

    correctif glitch graphique dans la structure du code de la camera

--- JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/camera.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/camera.cs
@@ -121,21 +121,21 @@ public class camera : MonoBehaviour {
 
 		if (InputManager.GetKeyAxis("Joystick X") > this.inputMinimumManette){
 
-			horizontal += ((InputManager.GetKeyAxis("Joystick X") - this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteX;
+			horizontal += (((InputManager.GetKeyAxis("Joystick X") - this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteX);
 
-		} else if(InputManager.GetKeyAxis("Joystick X") < - this.inputMinimumManette) {
+		} else if(InputManager.GetKeyAxis("Joystick X") < - this.inputMinimumManette){
 
-			horizontal += ((InputManager.GetKeyAxis("Joystick X") + this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteX;
+			horizontal += (((InputManager.GetKeyAxis("Joystick X") + this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteX);
 
 		}
 
 		if (InputManager.GetKeyAxis("Joystick Y") > this.inputMinimumManette){
 
-			vertical += ((InputManager.GetKeyAxis("Joystick Y") - this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteY;
+			vertical += (((InputManager.GetKeyAxis("Joystick Y") - this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteY);
 			
 		} else if (InputManager.GetKeyAxis("Joystick Y") < -this.inputMinimumManette){
 
-			vertical += ((InputManager.GetKeyAxis("Joystick Y") + this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteY;
+			vertical += (((InputManager.GetKeyAxis("Joystick Y") + this.inputMinimumManette) / (1.0f - this.inputMinimumManette)) * sensibiliteManetteY);
 			
 		}
 	}
