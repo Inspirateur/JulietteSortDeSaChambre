3f696e13c51c5ab9a20bb6bd4ce1f0bd899f2150
DESKTOP-GNH8QHQ\guill <guillaume.kay.morand@gmail.com>
2018-02-20 18:50:06 -0500

    mouvement de balancier des haches

--- JulietteSortDeSaChambre/Assets/SCRIPTS/DECORS/lameBalance.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/DECORS/lameBalance.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class lameBalance : MonoBehaviour {
+	public float vitesse = 0;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		transform.RotateAround(this.gameObject.transform.GetChild(0).position, Vector3.left, 500 * Time.deltaTime);
+		vitesse -= Mathf.Sin(Mathf.Deg2Rad*(this.transform.eulerAngles.z))*Time.deltaTime;
+		//Debug.Log (this.transform.eulerAngles.z+" -> "+Mathf.Cos(Mathf.Deg2Rad*(this.transform.eulerAngles.z))*Time.deltaTime);
+		this.transform.Rotate(Vector3.forward * vitesse);
 	}
 }
