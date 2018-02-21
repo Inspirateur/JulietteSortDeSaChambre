dec1d7bcecdb509958455c5a3b000f01358d9872
DESKTOP-K82QM6E\DrInf <drinfect@hotmail.fr>
2018-02-20 23:36:19 -0600

    Ajout script pour les barreau de fin de niveau et ajout d'une vitesse pour les pics

--- JulietteSortDeSaChambre/Assets/SCRIPTS/DECORS/Spikes.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/DECORS/Spikes.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour {
 
+	private Animator anim;
+
+
 	[Header("Temps avant la premiere action :")]
 	public float TimeBeforeStart;
 
@@ -12,20 +15,27 @@ public class Spikes : MonoBehaviour {
 
 	private bool StopSpike;
 
+	[Range(0.1f, 5)]
+	[Tooltip("multiplication de la vitesse de l'animation, pour une vitesse normale : 1")]
+	public float SpeedMultiplier = 1f;
+
 	// Use this for initialization
 	void Start () {
 		StopSpike = false;
 		StartCoroutine(WaitBeforeStart());
+		anim = gameObject.GetComponent<Animator> ();
+		anim.speed = anim.speed * SpeedMultiplier;
 	}
 
 	void Update() {
-		if (gameObject.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("BeginRepos") && !StopSpike) {
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("BeginRepos") && !StopSpike) {
 			StartCoroutine (WaitBeforeUp ());
 		}
+
 	}
 
 	void StopSpikes() {
-		gameObject.GetComponent<Animator> ().SetBool ("CanUp", false);
+		anim.SetBool ("CanUp", false);
 		StopSpike = true;
 	}
 
@@ -36,13 +46,14 @@ public class Spikes : MonoBehaviour {
 	IEnumerator WaitBeforeStart()
 	{
 		yield return new WaitForSeconds(TimeBeforeStart);
-		gameObject.GetComponent<Animator> ().SetBool ("CanUp", true);
+		anim.SetBool ("CanUp", true);
 	}
 
 	IEnumerator WaitBeforeUp()
 	{
-		gameObject.GetComponent<Animator> ().SetBool ("CanUp", false);
+		anim.SetBool ("CanUp", false);
 		yield return new WaitForSeconds(TimeRepos);
-		gameObject.GetComponent<Animator> ().SetBool ("CanUp", true);
+		anim.SetBool ("CanUp", true);
 	}
 }
+
