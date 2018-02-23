3b303fbd3997867458374fe7e51e824b5b2266cd
Lucas <kitolex.soluce@gmail.com>
2018-02-23 12:54:52 -0500

    desaffiche vie mort

--- JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/PrincesseVie.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/PrincesseVie.cs
@@ -61,12 +61,12 @@ public class PrincesseVie : MonoBehaviour {
 			Debug.Log ("GAME OVER");
 			gameover = true;
 			hudMort.afficheMort ();
-			/*SceneManager.LoadScene (scene.name);
+			SceneManager.LoadScene (scene.name);
 			GameControl.control.Load ();
 			Debug.Log(GameControl.control.listArmeTenu);
 			vie_courante = vie_max;
 			GameControl.control.vie = vie_courante;
-			GameControl.control.Save ();*/
+			GameControl.control.Save ();
 		}
 
 		if (Input.GetKeyDown (KeyCode.X)) {
