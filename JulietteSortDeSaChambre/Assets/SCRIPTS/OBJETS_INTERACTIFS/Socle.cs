f263c399f7c7898ad48dc096356a3c94d9c53127
DESKTOP-AA74124\Theo <theolerbeil@gmail.com>
2018-02-24 23:41:13 -0500

    ajouts arcs

--- JulietteSortDeSaChambre/Assets/SCRIPTS/OBJETS_INTERACTIFS/Socle.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/OBJETS_INTERACTIFS/Socle.cs
@@ -6,10 +6,27 @@ public class Socle : ObjetEnvironnemental {
 
 	public int BarreNumberToOpen;
 	public GameObject SocleEvenement;
+	private bool active;
+	public bool isReactivable;
 
-
+	void Start () {
+		active = true;
+	}
 	public override void Activation(){
-		this.gameObject.transform.GetChild (0).gameObject.SetActive(true);
-		SocleEvenement.GetComponent<barreMetalManager> ().DownBarre(BarreNumberToOpen);
+		if (active) {
+			this.gameObject.transform.GetChild (0).gameObject.SetActive (true);
+			SocleEvenement.GetComponent<barreMetalManager> ().DownBarre (BarreNumberToOpen);
+			active = false;
+		}
+	}
+
+	override
+	public EnumIconeInterraction getIconeInteraction(){
+
+		if (!active && !isReactivable) {
+			return EnumIconeInterraction.icone_null;
+		}
+
+		return EnumIconeInterraction.icone_default;
 	}
 }
