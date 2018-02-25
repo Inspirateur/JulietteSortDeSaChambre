37364181a3ce94e92487887fd946d3e05206d64d
Lucas <kitolex.soluce@gmail.com>
2018-02-25 18:57:39 -0500

    Modification socle need cristal pour l'utiliser

--- JulietteSortDeSaChambre/Assets/SCRIPTS/OBJETS_INTERACTIFS/Socle.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/OBJETS_INTERACTIFS/Socle.cs
@@ -7,26 +7,41 @@ public class Socle : ObjetEnvironnemental {
 	public int BarreNumberToOpen;
 	public GameObject SocleEvenement;
 	private bool active;
-	public bool isReactivable;
+	private PrincesseObjetProgression juliette;
+	public EnumObjetProgression obj;
 
 	void Start () {
+		juliette= GameObject.FindGameObjectWithTag("Player").GetComponent<PrincesseObjetProgression>();
 		active = true;
 	}
 	public override void Activation(){
 		if (active) {
-			this.gameObject.transform.GetChild (0).gameObject.SetActive (true);
-			SocleEvenement.GetComponent<barreMetalManager> ().DownBarre (BarreNumberToOpen);
-			active = false;
+			if(isActivable()){
+				this.gameObject.transform.GetChild (0).gameObject.SetActive (true);
+				SocleEvenement.GetComponent<barreMetalManager> ().DownBarre (BarreNumberToOpen);
+				active = false;
+				juliette.removeItem (obj);
+			}
 		}
+	}
+
+
+	public bool isActivable(){
+		return juliette.listObjet.ContainsKey(obj);
 	}
 
 	override
 	public EnumIconeInterraction getIconeInteraction(){
 
-		if (!active && !isReactivable) {
+		if (!active) {
 			return EnumIconeInterraction.icone_null;
 		}
+		if(isActivable()){
+			return EnumIconeInterraction.icone_default;
+		}else{
+			return EnumIconeInterraction.icone_non_default;
+		}
 
-		return EnumIconeInterraction.icone_default;
+
 	}
 }
