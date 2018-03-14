391369750e4662ea2d0b856542c2f4af8208b1bf
Fiesta87 <jb@pommeret.fr>
2018-03-14 17:28:51 -0400

    ménage commentaires

--- JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/SQUELETTE/SQL_Animations.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/SQUELETTE/SQL_Animations.cs
@@ -9,7 +9,6 @@ public class SQL_Animations {
 	// public static readonly string CHERCHER = "chercher";
 	// public static readonly string CHARGER = "charger";
 	public static readonly string ETRE_BLESSE = "etreBlesse";
-	// public static readonly string MOURIR = "mourir";
 	// public static readonly string COMBATTRE = "combattre";
 	public static readonly string GARDER = "garder";
 	// public static readonly string ATTAQUER_EMPALER = "attaquerEmpaler";
--- JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/SQUELETTE/SQL_E_Mourir.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/SQUELETTE/SQL_E_Mourir.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class SQL_E_Mourir : IA_Etat {
 
-	// public float delaiAvantDisparition;
-	// public AudioClip sonMort;
-
-	// private float actualDelai;
-	// private bool sonJoue;
-
 	public Transform prefabCadavre;
 
 	// Use this for initialization
@@ -21,23 +15,13 @@ public class SQL_E_Mourir : IA_Etat {
 	}
 
 	public override void entrerEtat() {
-		// anim.Play(SQL_Animations.MOURIR);
-		// rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
-		// actualDelai = Time.time + delaiAvantDisparition;
-		// sonJoue = false;
 		Instantiate(prefabCadavre, this.transform.position, this.transform.rotation);
 		this.gameObject.SetActive (false);
 	}
 
 	public override void faireEtat()
 	{
-		// if (!agent.getSoundEntity().isPlaying() && !sonJoue){
-		// 	agent.getSoundEntity().playOneShot(sonMort,1.0f);
-		// 	sonJoue = true;
-		// }
-		// if(Time.time >= actualDelai){
-		// 	this.gameObject.SetActive (false);
-		// }
+
 	}
 
 	public override void sortirEtat()
