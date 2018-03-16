f32c0662c399c2fbb810f2681aab050e85577fe0
Fiesta87 <jb@pommeret.fr>
2018-03-16 10:43:47 -0400

    squelette garde

--- JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/SQUELETTE/SQL_Animations.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MOBS/SQUELETTE/SQL_Animations.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SQL_Animations {
 
 	// public static readonly string MARCHER = "marcher";
-	// public static readonly string COURIR = "courir";
+	public static readonly string COURIR = "courir";
 	// public static readonly string CHERCHER = "chercher";
 	// public static readonly string CHARGER = "charger";
 	public static readonly string ETRE_BLESSE = "etreBlesse";
