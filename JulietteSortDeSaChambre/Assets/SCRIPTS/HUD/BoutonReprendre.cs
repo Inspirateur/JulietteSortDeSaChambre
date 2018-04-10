c0417edcff2916da1eea704871fe9685d71b4c5c
Lupusanghren <brisingr34130@gmail.com>
2018-04-10 18:09:35 -0500

    Ajout des Input pour les boutons

--- JulietteSortDeSaChambre/Assets/SCRIPTS/HUD/BoutonReprendre.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/HUD/BoutonReprendre.cs
@@ -6,20 +6,28 @@ using UnityEngine.EventSystems;// Required when using Event data.
 public class BoutonReprendre : MonoBehaviour, ISelectHandler, IDeselectHandler {
 
 	// Use this for initialization
+	private int selected;
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(selected==1 && (Input.GetButtonDown("Interagir")||Input.GetKeyDown(KeyCode.Return))){
+			onClick();
+		}else if(selected==2 && (Input.GetButtonDown("Interagir")||Input.GetKeyDown(KeyCode.Return))){
+			onClickQuitter();
+		}
 	}
 
 	public void onClick(){
 		GetComponentInParent<AffichagePause>().finPause();
+		Debug.Log("Reprendre");
 	}
 
 	public void onClickQuitter(){
+		//Application.Quit();
+		//LoadAsync de la scene
 		Debug.Log("quitter");
 	}
 
@@ -27,11 +35,18 @@ public class BoutonReprendre : MonoBehaviour, ISelectHandler, IDeselectHandler {
 		Debug.Log("OnSe;ect");
 		var color=new Color32(180,180,180,255);
 		GetComponentInChildren<UnityEngine.UI.Text>().color=color;
+		Debug.Log(eventData.selectedObject.name);
+		if(eventData.selectedObject.name=="Reprendre"){
+			selected=1;
+		}else if(eventData.selectedObject.name=="Quitter"){
+			selected=2;
+		}
 	}
 
 	public void OnDeselect(BaseEventData eventData){
 		Debug.Log("OnDese;ect");
 		var color=new Color32(255,255,255,255);
 		GetComponentInChildren<UnityEngine.UI.Text>().color=color;
+		selected=0;
 	}
 }
