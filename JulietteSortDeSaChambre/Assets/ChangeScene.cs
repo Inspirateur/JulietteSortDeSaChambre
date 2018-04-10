a41c1a7f57d64a223189015c3a80ad70007cea46
Guillaume Duminy <gduminy@etu.uqac.ca>
2018-04-10 18:25:04 -0400

    passage de la scene du niveau chambre au niveau 1

---
+++ JulietteSortDeSaChambre/Assets/ChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour {
+
+	    public string NomDeLaScene;
+		public string NomDeLaSceneChargement;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(){
+		Debug.Log("je passe ici");
+		PlayerPrefs.SetString("SceneToLoad", NomDeLaScene);
+        SceneManager.LoadScene(NomDeLaSceneChargement);	
+	}
+}
