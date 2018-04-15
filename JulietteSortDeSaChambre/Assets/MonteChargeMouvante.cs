863f823722f06120e6551794dc2a0d31031a8cb8
DrInfect <drinfect@hotmail.fr>
2018-04-14 23:31:41 -0500

    Correction monte charge avec des sons + mur niveau 1

---
+++ JulietteSortDeSaChambre/Assets/MonteChargeMouvante.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonteChargeMouvante : Evenement
+{
+
+	private AudioSource audioSource;
+
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	override
+	public void activation() {
+		GetComponent<MovingObject>().StartPlateformeBeggening();
+		audioSource.Play();
+	}
+
+	public void test() {
+
+	}
+
+	public void test2(int t) {
+
+	}
+
+	public int test3() {
+		return 1;
+	}
+}
