c41e57793a829aaecadfed6e6c8d09fc8a775c04
DrInfect <drinfect@hotmail.fr>
2018-04-14 19:05:47 -0500

    Correction retour au menuPrincipal dans les controles, la touche associer est Echap (et E aussi pour manette mais du coup sa marche aussi sur clavier)

---
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MAIN_MENU/CloseControlePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseControlePanel : MonoBehaviour {
+
+	public MenuPrincipal mp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Pause") || Input.GetButtonDown("Interagir")) {
+			mp.RetourMenuPrincipal();
+		}
+	}
+}
