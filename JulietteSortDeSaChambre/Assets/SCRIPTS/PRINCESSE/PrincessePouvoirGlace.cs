c3788794c52fa0481fde395977c9f35685c97606
Lupusanghren <brisingr34130@gmail.com>
2018-04-18 19:57:37 -0500

    Correction du pouvoir, on ne peut plus pouvoir quand on est en l'air

--- JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/PrincessePouvoirGlace.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/PRINCESSE/PrincessePouvoirGlace.cs
@@ -56,7 +56,7 @@ public class PrincessePouvoirGlace : PrincessePouvoir
     // Update is called once per frame
     void Update()
     {
-        if (((InputManager.GetButtonDown("pouvoirGlace") || Input.GetAxisRaw("pouvoirGlace")<-0.75f)&& canPower && isUnlocked) && deplacement.canUsePower)
+        if (((InputManager.GetButtonDown("pouvoirGlace") || Input.GetAxisRaw("pouvoirGlace")<-0.75f)&& canPower && isUnlocked) && deplacement.canUsePower && GetComponentInParent<PrincesseDeplacement>().isGrounded)
         {
             deplacement.AttaqueInteromput();
             animator.Play("IcePower");
