9f026fd9b11564f619e6675a60f68d81566cc661
gduminy <guillaume.duminy@etu.unilim.fr>
2018-04-20 17:17:01 -0400

    correction bug du troll

--- JulietteSortDeSaChambre/Assets/SCRIPTS/OBJETS_INTERACTIFS/SpawnObjectFromMobs.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/OBJETS_INTERACTIFS/SpawnObjectFromMobs.cs
@@ -24,8 +24,8 @@ public class SpawnObjectFromMobs : MonoBehaviour {
 				PremiereFois = true;
 			} else if (!vie.estEnVie () && PremiereFois == true) {
 				ObjectSpawn.transform.position = this.transform.position;
-				this.GetComponent<BoxCollider> ().enabled = false;
-				Destroy (GetComponent<Rigidbody> ());
+				//this.GetComponent<BoxCollider> ().enabled = false;
+				//Destroy (GetComponent<Rigidbody> ());
 				ObjectSpawn.SetActive (true);
 				theta = Random.value * 360;
 				ObjectSpawn.GetComponent<Rigidbody> ().AddForce (new Vector3 (Mathf.Cos (theta) * 200, 200, Mathf.Sin (theta) * 200));
