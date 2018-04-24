c123178f95410ed6c65c657903c3f4edb46ace5c
DrInfect <drinfect@hotmail.fr>
2018-04-23 19:10:21 -0500

    Correction input menu

--- JulietteSortDeSaChambre/Assets/SCRIPTS/MAIN_MENU/CloseControlePanel.cs
+++ JulietteSortDeSaChambre/Assets/SCRIPTS/MAIN_MENU/CloseControlePanel.cs
@@ -13,7 +13,7 @@ public class CloseControlePanel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Pause") || Input.GetButtonDown("Interagir")) {
+		if (Input.GetButtonDown("Cancel")) {
 			mp.RetourMenuPrincipal();
 		}
 	}
